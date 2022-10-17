6bb678d36681b38e6d9da6fbeaf44f01113d24af
Robert <rzarkova@uccs.edu>
2022-10-17 22:35:38 +0000

    Added scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+end
+
+Rails.application.routes.draw do
+  root "projects#index"
+  resources :projects
 end
