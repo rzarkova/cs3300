f92a55dfcd24c8d3d793fdf991a35ef060d48944
Robert Zarkovacki <rzarkova@uccs.edu>
2022-11-10 22:40:54 +0000

    Got testing percent above 70

--- spec/controller/project_spec.rb
+++ spec/controller/project_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe ProjectsController, type: :controller do
       expect(response).to be_successful
     end
   end
+
+  context "GET #new" do
+    it "returns a success response" do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  context "GET #edit" do
+    let!(:project) { Project.create(title: "Test title", description: "Test description") }
+    it "returns a success response" do
+      get :edit, params: { id: project }
+      expect(response).to be_successful
+    end
+  end
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/' # for rspec
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -60,10 +66,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
-require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter '/bin/'
-  add_filter '/db/'
-  add_filter '/spec/' # for rspec
 end
